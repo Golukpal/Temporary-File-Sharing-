b23733b699e5c535d0a41e902a991b9b1607bae5
Golu Kumar Pal <golukpal@ssipmt.com>
2026-08-15 01:18:34 +0530

    add config and connect

---
+++ internal/database/postgres.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	"github.com/Golukpal/file-sharing/internal/config"
+)
+
+func Connect(cfg config.Config) (*pgxpool.Pool, error) {
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s",
+		cfg.DBUser,
+		cfg.DBPassword,
+		cfg.DBHost,
+		cfg.DBPort,
+		cfg.DBName,
+	)
+
+	pool, err := pgxpool.New(context.Background(), dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := pool.Ping(context.Background()); err != nil {
+		return nil, err
+	}
+
+	return pool, nil
+}
