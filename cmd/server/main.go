4f42616f43906ec196ebd817bc2bae8410902ae8
Golu Kumar Pal <golukpal@ssipmt.com>
2026-08-16 23:42:48 +0530

    update main file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 
 	"github.com/Golukpal/file-sharing/internal/config"
 	"github.com/Golukpal/file-sharing/internal/database"
+	"github.com/Golukpal/file-sharing/internal/handler"
+	"github.com/Golukpal/file-sharing/internal/repository"
+	"github.com/Golukpal/file-sharing/internal/service"
 )
 
 func main() {
 	cfg := config.Load()
+
+	if err := os.MkdirAll("storage", 0755); err != nil {
+		log.Fatal("failed to create storage directory:", err)
+	}
 
 	db, err := database.Connect(cfg)
 	if err != nil {
@@ -19,6 +27,12 @@ func main() {
 
 	defer db.Close()
 
+	fileRepository := repository.NewFileRepository(db)
+
+	fileService := service.NewFileService(fileRepository)
+
+	fileHandler := handler.NewFileHandler(fileService)
+
 	router := gin.Default()
 
 	router.GET("/health", func(c *gin.Context) {
@@ -26,6 +40,8 @@ func main() {
 			"status": "ok",
 		})
 	})
+
+	router.POST("/files", fileHandler.Upload)
 
 	log.Println("server running on port", cfg.AppPort)
 
