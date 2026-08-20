fdf43de881ea41f95b8bc1bedb56230d27f51dcd
Golu Kumar Pal <golukpal@ssipmt.com>
2026-08-20 23:50:39 +0530

    add centralized response for handler

---
+++ internal/handler/response.go
@@ -0,0 +1,13 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func errorResponse(
+	c *gin.Context,
+	status int,
+	message string,
+) {
+	c.JSON(status, gin.H{
+		"error": message,
+	})
+}
